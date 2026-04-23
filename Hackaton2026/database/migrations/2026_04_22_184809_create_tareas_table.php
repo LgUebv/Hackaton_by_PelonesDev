328cd2a309c71d2e44fda22af4ec09d9363367a8
Eueuito <lguebv@gmail.com>
2026-04-23 10:17:57 -0600

    chiquito pero picoso

--- Hackaton2026/database/migrations/2026_04_22_184809_create_tareas_table.php
+++ Hackaton2026/database/migrations/2026_04_22_184809_create_tareas_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('tareas', function (Blueprint $table) {
             $table->id();
             $table->string('nombreTarea');
-            $table->string('descripcion');
+            $table->string('descripcion')->nullable();
             $table->dateTime('fechaHora')->unique();
             $table->time('tiempoAsignado');
             $table->unsignedInteger('importancia');
